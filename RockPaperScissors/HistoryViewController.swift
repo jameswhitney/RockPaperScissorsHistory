ae097e995b5aa8f9641d9b9881f4009266cf8f4b
James Whitney <jamesdrums@mac.com>
2018-02-08 08:25:46 -0500

    Added HistoryViewController to show win/losses.

---
+++ RockPaperScissors/HistoryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryViewController.swift
+//  RockPaperScissors
+//
+//  Created by James Whitney on 2/8/18.
+//  Copyright © 2018 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HistoryViewController: UIViewController {
+    
+}
